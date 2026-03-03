18efc4bb9b11ec4aa72b905c9fe98bf1a13d860f
Thalysson Delano <167926095+thalyssondel@users.noreply.github.com>
2026-03-03 18:29:52 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, send_from_directory, render_template
+
+app = Flask(__name__, template_folder='.')
+
+# 1. Rota para liberar a pasta de vídeos
+@app.route('/videos/<path:filename>')
+def serve_videos(filename):
+    return send_from_directory('videos', filename)
+
+# 2. Rota para liberar a pasta de imagens
+@app.route('/images/<path:filename>')
+def serve_imagens(filename):
+    return send_from_directory('images', filename)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
